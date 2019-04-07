04bd92e96b899b1cc70dd681272b88d51ff4e155
Nash Brancic <nashfft@gmail.com>
2019-04-07 02:42:30 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -1,15 +1,17 @@
 require 'pry'
 
-class MusicLibraryController
+  class MusicLibraryController
   
-  def initialize(path = "./db/mp3s")
-    MusicImporter.new(path).import
-  end
+    def initialize(path = "./db/mp3s")
+      MusicImporter.new(path).import
+    end
   
-def call
-  input = ""
+  def call
+    
+    input = ""
 
   while input != "exit"
+      
       puts "Welcome to your music library!"
       puts "To list all of your songs, enter 'list songs'."
       puts "To list all of the artists in your library, enter 'list artists'."
@@ -22,20 +24,20 @@ def call
 
       input = gets.strip
 
-      case input
-     when "list songs"
+    case input
+      when "list songs"
        list_songs
-     when "list artists"
+      when "list artists"
        list_artists
-     when "list genres"
+      when "list genres"
        list_genres
-     when "list artist"
+      when "list artist"
        list_songs_by_artist
-     when "list genre"
+      when "list genre"
        list_songs_by_genre
      when "play song"
        play_song
      end
    end
- end
+  end
  end
