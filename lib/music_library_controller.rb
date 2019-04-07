afff164262d9cdaccddfaf2d578e521ceb0a347d
Nash Brancic <nashfft@gmail.com>
2019-04-07 02:46:35 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -6,12 +6,11 @@ require 'pry'
       MusicImporter.new(path).import
     end
   
-  def call
-    
-    input = ""
+def call
+  
+  input = ""
 
   while input != "exit"
-      
       puts "Welcome to your music library!"
       puts "To list all of your songs, enter 'list songs'."
       puts "To list all of the artists in your library, enter 'list artists'."
@@ -24,39 +23,50 @@ require 'pry'
 
       input = gets.strip
 
-    case input
-      when "list songs"
+      case input
+     when "list songs"
        list_songs
-      when "list artists"
+     when "list artists"
        list_artists
-      when "list genres"
+     when "list genres"
        list_genres
-      when "list artist"
+     when "list artist"
        list_songs_by_artist
-      when "list genre"
+     when "list genre"
        list_songs_by_genre
      when "play song"
        play_song
      end
    end
-  end
-  
-  def list_songs
-    Song.all.sort {|n,b| n.name <=> b.name}.each.with_index(1) do |song, index|
+ end
+
+def list_songs
+  Song.all.sort {|x,y| x.name <=> y.name}.each.with_index(1) do |song, index|
     puts "#{index}. #{song.artist.name} - #{song.name} - #{song.genre.name}"
-   end
   end
-  
-  def list_artists
-      Artist.all.sort {|n,b| n.name <=> b.name}.each.with_index(1) do |artist, index|
+end
+
+def list_artists
+  Artist.all.sort {|x,y| x.name <=> y.name}.each.with_index(1) do |artist, index|
     puts "#{index}. #{artist.name}"
   end
- end
- 
-  def list_genres
-      Genre.all.sort {|n,b| n.name <=> b.name}.each.with_index(1) do |genre, index|
+end
+
+def list_genres
+  Genre.all.sort {|x,y| x.name <=> y.name}.each.with_index(1) do |genre, index|
     puts "#{index}. #{genre.name}"
   end
+end
+
+def list_songs_by_artist
+  puts "Please enter the name of an artist:"
+  input = gets.strip
+
+  if artist = Artist.find_by_name(input)
+    artist.songs.sort{ |x, y| x.name <=> y.name }.each.with_index(1) do |song, index|
+        puts "#{index}. #{song.name} - #{song.genre.name}"
+      end
+    end
 end
   
   
